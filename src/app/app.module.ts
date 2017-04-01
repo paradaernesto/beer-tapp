a44cb624387f4e1f0c631fe373d041af2d66bc73
Ernesto Parada <ernesto.parada@patagonian.it>
2017-04-01 10:51:49 -0300

    Adds components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,22 @@ import { HttpModule } from '@angular/http';
 import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductionComponent } from './components/production/production.component';
+import { SalesComponent } from './components/sales/sales.component';
+import { StockComponent } from './components/stock/stock.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProductionComponent,
+    SalesComponent,
+    StockComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
